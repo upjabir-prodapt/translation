4e7f9dafe1c9b3e2caae74076d05a3b692689913
upjabir <upjabir@gmail.com>
2026-03-17 04:11:18 +0530

    removed schemas

--- src/babeldoc/__init__.py
+++ src/babeldoc/__init__.py
@@ -1,34 +1,8 @@
 __version__ = "0.5.23"
 
-# New async translation service API
 from babeldoc.format.pdf.high_level import async_translate
-from babeldoc.new_main import cancel_job
-from babeldoc.new_main import cleanup_job
-from babeldoc.new_main import cleanup_job_directory
-from babeldoc.new_main import create_translation_job
-from babeldoc.new_main import get_job_status
-from babeldoc.new_main import list_jobs
-from babeldoc.new_main import run_translation_job
-from babeldoc.new_main import translate_pdf
-from schemas.babeldoc_schemas import TranslationConfigSchema
-from schemas.babeldoc_schemas import TranslationJobStatus
-from schemas.babeldoc_schemas import TranslationModelSchema
 
 __all__ = [
-    # New async service
-    "create_translation_job",
-    "run_translation_job",
-    "get_job_status",
-    "list_jobs",
-    "cancel_job",
-    "cleanup_job",
-    "cleanup_job_directory",
-    "translate_pdf",
     "async_translate",
-    # Schemas
-    "TranslationConfigSchema",
-    "TranslationModelSchema",
-    "TranslationJobStatus",
-    # Legacy
     "__version__",
 ]
